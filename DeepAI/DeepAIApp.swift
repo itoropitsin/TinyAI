1ac7f39bd3f009b1f21dd034b2e0015680625c38
itoropitsin <i.toropitsin@gmail.com>
2025-12-12 17:00:26 +0100

    Initial Commit

---
+++ DeepAI/DeepAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepAIApp.swift
+//  DeepAI
+//
+//  Created by Ivan on 12/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeepAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeepAITests/DeepAITests.swift
@@ -0,0 +1,17 @@
+//
+//  DeepAITests.swift
+//  DeepAITests
+//
+//  Created by Ivan on 12/12/2025.
+//
+
+import Testing
+@testable import DeepAI
+
+struct DeepAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
